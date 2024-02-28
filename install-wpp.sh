e4380ea2dfcfa412ad03469fa927fa2b019c0365
Jonathan Lima <89461400+limatecnologia@users.noreply.github.com>
2024-02-28 11:19:31 -0300

    Update install-wpp.sh

--- install-wpp.sh
+++ install-wpp.sh
@@ -68,19 +68,19 @@ npm install moment-timezone
 
 
 # Baixe os arquivos do GitHub
-wget -O index.js https://raw.githubusercontent.com/limatecnologia/limatec/main/index.js
-wget -O limazap.js https://raw.githubusercontent.com/limatecnologia/limatec/main/limazap.js
-wget -O limazap https://raw.githubusercontent.com/limatecnologia/limatec/main/limazap
+wget -O index.js https://raw.githubusercontent.com/limatecnologia/whatsapp/main/index.js
+wget -O limazap.js https://raw.githubusercontent.com/limatecnologia/whatsapp/main/limazap.js
+wget -O limazap https://raw.githubusercontent.com/limatecnologia/whatsapp/main/limazap
 chmod +x limazap
 clear
 sleep 5
 clear
 
 # Atualize o node
-echo "Atualizando o node"
-curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash && source ~/.profile && nvm install 18 && nvm use 18
-sleep 2
-clear
+#echo "Atualizando o node"
+#curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash && source ~/.profile && nvm install 18 && nvm use 18
+#sleep 2
+#clear
 
 # Limpeza
 cd /opt/limazap
